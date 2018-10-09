6d2452e58b511e75a93eb5d327399eed5d3e1ca1
Babu Thomas <babuthomas138@gmail.com>
2018-10-09 12:36:28 +0530

    Another solution for reverse-integer problem

--- reverse-integer_solve.cpp
+++ reverse-integer_solve.cpp
@@ -12,3 +12,33 @@ int Solution::reverse(int A) {
     
     return result;
 }
+
+
+// Better solution
+
+// Time - O(Log N), Space - O(1)
+int Solution::reverse(int A) {
+    int result = 0;
+    bool is_negative = false;
+    if(A < 0) {
+        is_negative = true;
+        A *= -1;
+    }
+
+    while(A > 0) {
+        int digit = A % 10;
+        // Check if adding a new digit will cause the reverse to overflow
+        if(result > (INT_MAX / 10) || (result == (INT_MAX / 10) && digit > (INT_MAX % 10))) {
+            return 0;
+        }
+
+        result = result * 10 + digit;
+        A /= 10;
+    }
+
+    if(is_negative) {
+        return -result;
+    }
+
+    return result;
+}
--- reverse-integer_test.cpp
+++ reverse-integer_test.cpp
@@ -11,6 +11,7 @@ using namespace std;
         std::cout << #x << ": " << x << "\n";                                  \
     } while (0)
 
+// Time - O(Log N), Space - O(1)
 int solve(int A) {
     long long result = 0;
     while (A != 0) {
@@ -25,12 +26,40 @@ int solve(int A) {
     return result;
 }
 
+// Time - O(Log N), Space - O(1)
+int solve_better(int A) {
+    int result = 0;
+    bool is_negative = false;
+    if(A < 0) {
+        is_negative = true;
+        A *= -1;
+    }
+
+    while(A > 0) {
+        int digit = A % 10;
+        // Check if adding a new digit will cause the reverse to overflow
+        if(result > (INT_MAX / 10) || (result == (INT_MAX / 10) && digit > (INT_MAX % 10))) {
+            return 0;
+        }
+
+        result = result * 10 + digit;
+        A /= 10;
+    }
+
+    if(is_negative) {
+        return -result;
+    }
+
+    return result;
+}
+
 int main() {
     // ios::sync_with_stdio(0);
     // cin.tie(0);
 
     int n = -199999998;
     cout << solve(n) << "\n";
+    cout << solve_better(n) << "\n";
 
     return 0;
 }
