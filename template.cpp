b40171147eba997ffd71a9a22332e11fc58ad144
Babu Thomas <babuthomas138@gmail.com>
2018-10-31 13:05:42 +0530

    Add starter code for new solutions

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG(x)                                                               \
+    do {                                                                       \
+        std::cout << #x << ": " << x << " ";                                   \
+    } while (0)
+#define DEBUGL(x)                                                              \
+    do {                                                                       \
+        std::cout << #x << ": " << x << "\n";                                  \
+    } while (0)
+
+#define MOD 1000000007
+
+template <typename T> void print_vector(vector<T> a) {
+    for (auto &i : a) {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
+
+template <typename T> void print_matrix_2d(vector<vector<T>> a) {
+    int r = 0;
+    for (auto &i : a) {
+        cout << r << ": ";
+        for (auto &j : i) {
+            cout << j << " ";
+        }
+        r++;
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+int main() {
+    // ios::sync_with_stdio(0);
+    // cin.tie(0);
+
+    return 0;
+}
