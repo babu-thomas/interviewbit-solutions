b7e0589e5f4af85f98d5553f791b80091ba7ec63
Babu Thomas <babuthomas138@gmail.com>
2018-11-19 13:01:29 +0530

    Another solution for smallest-sequence-with-given-primes problem

--- smallest-sequence-with-given-primes_solve.cpp
+++ smallest-sequence-with-given-primes_solve.cpp
@@ -21,3 +21,29 @@ vector<int> Solution::solve(int A, int B, int C, int D) {
 
     return result;
 }
+
+
+// Time - O(N * P), Space - O(N + P)
+// where N is the number of integers to generate and P is the number of primes
+// given.
+vector<int> Solution::solve(int A, int B, int C, int D) {
+    vector<int> primes{A, B, C};
+    int nums_count = D;
+    vector<int> candidate_terms = primes;
+    vector<int> term_indices(primes.size(), 0);
+    vector<int> result;
+    for (int count = 0; count < nums_count; count++) {
+        int next_term =
+            *min_element(candidate_terms.begin(), candidate_terms.end());
+        result.push_back(next_term);
+
+        for (int t = 0; t < candidate_terms.size(); t++) {
+            if (candidate_terms[t] == next_term) {
+                candidate_terms[t] = primes[t] * result[term_indices[t]];
+                term_indices[t]++;
+            }
+        }
+    }
+
+    return result;
+}
--- smallest-sequence-with-given-primes_test.cpp
+++ smallest-sequence-with-given-primes_test.cpp
@@ -57,7 +57,30 @@ vector<int> solve(const vector<int> &A, int B) {
     return result;
 }
 
+// Time - O(N * P), Space - O(N + P)
+// where N is the number of integers to generate and P is the number of primes
+// given.
+vector<int> solve_linear(const vector<int> &A, int B) {
+    const vector<int> &primes = A;
+    int nums_count = B;
+    vector<int> candidate_terms = primes;
+    vector<int> term_indices(primes.size(), 0);
+    vector<int> result;
+    for (int count = 0; count < nums_count; count++) {
+        int next_term =
+            *min_element(candidate_terms.begin(), candidate_terms.end());
+        result.push_back(next_term);
 
+        for (int t = 0; t < candidate_terms.size(); t++) {
+            if (candidate_terms[t] == next_term) {
+                candidate_terms[t] = primes[t] * result[term_indices[t]];
+                term_indices[t]++;
+            }
+        }
+    }
+
+    return result;
+}
 
 int main() {
     // ios::sync_with_stdio(0);
@@ -71,6 +94,9 @@ int main() {
     int nums_count = 10;
     print_vector(solve(primes1, nums_count));
     print_vector(solve(primes2, nums_count));
+
+    print_vector(solve_linear(primes1, nums_count));
+    print_vector(solve_linear(primes2, nums_count));
 
     return 0;
 }
